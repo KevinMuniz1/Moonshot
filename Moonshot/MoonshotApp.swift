7ec41d468f8cd2b779f273fceac9fdbf2b2eae0f
KevinMuniz1 <munizkevin@outlook.com>
2024-01-28 17:08:17 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kevin Muniz on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
