a2b11926125b78bd0a35e9004fa3dcb032869079
KevinMuniz1 <munizkevin@outlook.com>
2024-02-25 15:49:07 -0500

    added another view for the detail view

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Kevin Muniz on 2/25/24.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
