b09cf5143865905bed5f721f83dd30e2596843d2
KevinMuniz1 <munizkevin@outlook.com>
2024-02-25 18:01:01 -0500

    finished integrating the detail view for the astronauts, users can now click on the view and read more about them.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Kevin Muniz on 2/25/24.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
